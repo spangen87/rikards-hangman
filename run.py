944463f653fd0627ca38e537f36b444499143d14
spangen87 <rikard.spangmyr@bestlas.se>
2022-06-21 10:48:03 +0000

    Add text file with words and function to get one random

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def get_word():
+    """
+    Gets a random word from words.txt
+    """
+    random_word = random.choice(open("words.txt", "r").read().split('\n'))
+    print(random_word.upper())
+
+get_word()
+
+# Logo for the game. Generated with: https://www.ascii-art-generator.org/
+print(
+    """
+ _____  _ _                 _ _       _    _                                         
+|  __ \(_) |               | ( )     | |  | |                                        
+| |__) |_| | ____ _ _ __ __| |/ ___  | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+|  _  /| | |/ / _` | '__/ _` | / __| |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | \ \| |   < (_| | | | (_| | \__ \ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+|_|  \_\_|_|\_\__,_|_|  \__,_| |___/ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                                                          __/ |                      
+                                                         |___/        
+    """
+)
+               
