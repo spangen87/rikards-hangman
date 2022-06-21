23bf75f5c0d11f6e2a93cc9118936431a1571362
spangen87 <rikard.spangmyr@bestlas.se>
2022-06-21 12:28:44 +0000

    Add function for menu choices and welcome message

--- run.py
+++ run.py
@@ -1,13 +1,5 @@
 import random
 
-def get_word():
-    """
-    Gets a random word from words.txt
-    """
-    random_word = random.choice(open("words.txt", "r").read().split('\n'))
-    print(random_word.upper())
-
-get_word()
 
 # Logo for the game. Generated with: https://www.ascii-art-generator.org/
 print(
@@ -22,4 +14,41 @@ print(
                                                          |___/        
     """
 )
+
+#Welcoming the player to the game
+print("\nWelcome to Rikard's Hangman!")
+name = input("Enter your name:")
+print(f"Good Luck, {name}!")
+
+
+def menu():
+    """
+    Presents choices to the player
+    """
+    print("Press 1 to start the game")
+    print("Press 2 to show the rules")
+    print("Press 3 to set difficulty")
+    choice = input("Enter number:")
+    if choice == "1":
+        print("start_game")
+    elif choice == "2":
+        print("show_rules")
+    elif choice == "3":
+        print("choose_level")
+    else:
+        print("Please choose 1,2 or 3.")            
+
+menu()
+
+
+
+def get_word():
+    """
+    Gets a random word from words.txt
+    """
+    random_word = random.choice(open("words.txt", "r").read().split('\n'))
+    print(random_word.upper())
+
+get_word()
+
                
