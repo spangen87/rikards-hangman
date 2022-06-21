775b979d022c17fe2ed0394cdc49370de3b2c7b9
spangen87 <rikard.spangmyr@bestlas.se>
2022-06-21 13:56:04 +0000

    Add function to show the rules with way to get back to menu

--- run.py
+++ run.py
@@ -15,10 +15,40 @@ print(
     """
 )
 
-#Welcoming the player to the game
+# Welcoming the player to the game
 print("\nWelcome to Rikard's Hangman!")
 name = input("Enter your name:")
 print(f"Good Luck, {name}!")
+
+
+def get_word():
+    """
+    Gets a random word from words.txt
+    """
+    random_word = random.choice(open("words.txt", "r").read().split('\n'))
+    print(random_word.upper())
+
+
+get_word()
+
+
+def show_rules():
+    print(
+        """
+        Choose a letter from A to Z. If it's not in the word, you loose a life.\n
+        If it's correct it will show in the word.\n
+        The game is over if you loose all lives.\n
+        You win if you get all letters right!
+        """
+        )
+    input("Press enter to return to menu")
+    menu()
+
+
+def main():
+    """
+    Runs the game
+    """               
 
 
 def menu():
@@ -30,25 +60,16 @@ def menu():
     print("Press 3 to set difficulty")
     choice = input("Enter number:")
     if choice == "1":
-        print("start_game")
+        main()
     elif choice == "2":
-        print("show_rules")
+        show_rules()
     elif choice == "3":
         print("choose_level")
     else:
-        print("Please choose 1,2 or 3.")            
+        print("Please choose 1,2 or 3.")
+        menu()
+
 
 menu()
 
 
-
-def get_word():
-    """
-    Gets a random word from words.txt
-    """
-    random_word = random.choice(open("words.txt", "r").read().split('\n'))
-    print(random_word.upper())
-
-get_word()
-
-               
